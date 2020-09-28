32fe38cc41b972739a377d2c7493b3e365591ff8
chigozieorunta <chigozieorunta@yahoo.com>
2020-09-29 00:06:56 +0100

    Added Hero components to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,21 @@
 import React from "react"
 import Layout from "../layout/layout"
 
+import HeroTitle from "../components/hero-title"
+import HeroExcerpt from "../components/hero-excerpt"
+import HeroContainer from "../components/hero-container"
+
 const HomePage = () => {
   return (
     <Layout>
-      <p>Hello World!</p>
+      <HeroContainer>
+        <HeroTitle>Chigozie Orunta</HeroTitle>
+        <HeroExcerpt>
+          Hi, I'm a <em>full stack developer</em> who works with
+          <em>React.js</em> on the front-end and <em>PHP/Laravel</em> on the
+          back-end!
+        </HeroExcerpt>
+      </HeroContainer>
     </Layout>
   )
 }
