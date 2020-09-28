87900342f4f1b55d9ece7177edf977abf6028f7a
chigozieorunta <chigozieorunta@yahoo.com>
2020-09-28 23:18:36 +0100

    Added Nunito font and weights

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`nunito:200,300,400,500,600,700,800,900`],
+        display: "swap",
+      },
+    },
+  ],
 }
