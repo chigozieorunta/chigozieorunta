14d5f625b787256bf0601a57529f71237eb6163b
chigozieorunta <chigozieorunta@yahoo.com>
2020-09-29 02:55:07 +0100

    Modified Hero container for className attribute

--- src/components/hero-container.js
+++ src/components/hero-container.js
@@ -2,12 +2,12 @@ import React from "react"
 
 const HeroContainer = props => {
   return (
-    <section class="d-flex vh-100 align-items-center hero-section">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-sm-10 offset-sm-1">
-            <div class="row">
-              <div class="col-xl-6">{props.children}</div>
+    <section className="d-flex vh-100 align-items-center hero-section">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-10 offset-sm-1">
+            <div className="row">
+              <div className="col-xl-6">{props.children}</div>
             </div>
           </div>
         </div>
