ab4d300d1c89caad1ac303e0a540189e578db428
chigozieorunta <chigozieorunta@yahoo.com>
2020-09-29 02:55:59 +0100

    Added Header sub-components to Header

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,7 +1,18 @@
 import React from "react"
 
+import HeaderLogo from "../components/header-logo"
+import HeaderSocial from "../components/header-social"
+import HeaderContainer from "../components/header-container"
+import HeaderNavigation from "../components/header-navigation"
+
 const Header = () => {
-  return <></>
+  return (
+    <HeaderContainer>
+      <HeaderLogo>Chigozie Orunta</HeaderLogo>
+      <HeaderNavigation />
+      <HeaderSocial />
+    </HeaderContainer>
+  )
 }
 
 export default Header
