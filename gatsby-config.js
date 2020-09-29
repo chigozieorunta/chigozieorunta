a81b84805823066fc72520e00a47454d434a2513
chigozieorunta <chigozieorunta@yahoo.com>
2020-09-29 05:03:49 +0100

    Changed base font to Poppins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`nunito:200,300,400,500,600,700,800,900`],
+        fonts: [`poppins:200,300,400,500,600,700,800,900`],
         display: "swap",
       },
     },
