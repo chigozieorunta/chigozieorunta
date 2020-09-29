cd4eb99461d599bb63da6f8de073588eab18018e
chigozieorunta <chigozieorunta@yahoo.com>
2020-09-29 05:03:28 +0100

    Modified Home page, removed major chunks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,56 @@
 import React from "react"
 import Layout from "../layout/layout"
 
-import HeroTitle from "../components/hero-title"
 import HeroButton from "../components/hero-button"
 import HeroExcerpt from "../components/hero-excerpt"
 import HeroContainer from "../components/hero-container"
 
-const HomePage = () => {
+const ContactPage = () => {
   return (
     <Layout>
       <HeroContainer>
-        <HeroTitle>Chigozie Orunta</HeroTitle>
         <HeroExcerpt>
-          Hi, I'm a <em>full stack developer</em> who works with <em>React</em>{" "}
-          and <em>PHP/Laravel</em>...
+          <p className="mb-0">Hi.</p>
+          <p className="mb-0">
+            I'm <em>Chigozie Orunta</em>.
+          </p>
+          <p className="mb-0">
+            A <em>full stack developer</em>
+          </p>
+          <p>
+            based in <span>Lagos, Nigeria</span>.
+          </p>
         </HeroExcerpt>
         <HeroButton>View Portfolio</HeroButton>
+        <div style={{ fontSize: "20px", marginTop: "2em", fontWeight: "200" }}>
+          <p>My skillset and interests includes:</p>
+          <ul className="hero-skills">
+            <li>React</li>
+            <li>Bootstrap</li>
+            <li>Tailwind CSS</li>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>Redux</li>
+            <li>Context API</li>
+            <li>PHP</li>
+            <li>MySQL</li>
+            <li>Laravel</li>
+            <li>OAuth</li>
+            <li>Stripe</li>
+            <li>Docker</li>
+            <li>Composer</li>
+            <li>Packagist</li>
+            <li>WordPress</li>
+            <li>Themes</li>
+            <li>Plugins</li>
+            <li>Design Patterns</li>
+            <li>Unit Testing</li>
+            <li>Object Oriented Programming</li>
+          </ul>
+        </div>
       </HeroContainer>
     </Layout>
   )
 }
 
-export default HomePage
+export default ContactPage
