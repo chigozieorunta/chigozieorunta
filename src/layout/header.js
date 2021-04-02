0ec4d2439f602fd93135ff6593fdfc30ab0d432c
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-04-02 22:44:12 +0100

    Refactored empty Header

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,18 +1,5 @@
 import React from "react"
 
-import HeaderLogo from "../components/header-logo"
-import HeaderSocial from "../components/header-social"
-import HeaderContainer from "../components/header-container"
-import HeaderNavigation from "../components/header-navigation"
-
-const Header = () => {
-  return (
-    <HeaderContainer>
-      <HeaderLogo>Chigozie Orunta</HeaderLogo>
-      <HeaderNavigation />
-      <HeaderSocial />
-    </HeaderContainer>
-  )
-}
+const Header = () => <></>
 
 export default Header
