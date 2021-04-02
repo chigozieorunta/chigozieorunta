32c2924b2054810f62903dd1ff86739b7b1f3089
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-04-02 22:44:27 +0100

    Removed Header components

--- src/components/header-container.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const HeaderContainer = props => {
-  return <header className="header-section">{props.children}</header>
-}
-
-export default HeaderContainer
--- src/components/header-social.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const HeaderSocial = () => {
-  return <ul></ul>
-}
-
-export default HeaderSocial
