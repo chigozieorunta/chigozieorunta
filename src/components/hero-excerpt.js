e1d1a4a43298c1d30a13b96d06e66780e8a21f8c
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-04-04 02:52:11 +0100

    Removed old components

--- src/components/hero-excerpt.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const HeroExcerpt = props => {
-  return <h1 className="hero-excerpt">{props.children}</h1>
-}
-
-export default HeroExcerpt
--- src/components/hero-title.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const HeroTitle = props => {
-  return <h1 className="hero-title">{props.children}</h1>
-}
-
-export default HeroTitle
